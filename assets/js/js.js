ee06484749e7fec26b6f00017d5998ea2fb49072
Lillian Chan <lilchan717@gmail.com>
2021-06-17 22:20:04 -0500

    Draft 4 - working on question buttons in JS.

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,18 +3,32 @@ var timer = 0;
 var secLeft;
 var timerId;
 
-var questions = "";
+var questions = document.getElementById("questions");
+var answers = document.getElementById("answers");
+var startBtn = document.getElementById("start");
+var submitBtn = document.getElementById("submit");
+
 var score = 0;
 var index = 0;
 
 
-// function startQuiz(start) {
-//     let timer = setInterval(function () {
-//         time.textContent = secondsLeft;   //initial start time
+function startQuiz(start) {
+    timerEl.textContent = time;
+    var firstPage = document.getElementById("header");
+    firstPage.setAttribute("class", "start hide");
+
+    questionsEl.setAttribute("class", " ");
+    timerId = setInterval(function () {
+        time();   //initial start time
+    }, 1000);
+        timerEl.textContent = time;
+
+        getQ();
+    }
 //         secondsLeft--;
 //         if (secLeft === 0) {
 //             clearInterval(timer);
-          
+
 //         } else {
 //             time.textContent = secondsLeft   //new time is updated after each second
 //         }
@@ -23,37 +37,48 @@ var index = 0;
 
 // let timerInterval = setInterval(function () {
 
+function getQ() {
+    var currentQ = questions[currentQIndex];
+    questions.index[0].textContent = currentQ.title;
+    index++;
+}
+
+for (var i = 0; i < currentQ.options.length; i++) {
+    var choice = document.createElement("submit")
+
+
+
 
 function quizEnd() {
-    clearInterval(timerId);
-    timerEl.textContent = time;
+            clearInterval(timerId);
+            timerEl.textContent = time;
 
-    var lastScreenEl = 
+            var lastScreenEl = 
+    } 
 }
 
 
 function timer() {
-    time--;
-    timerEl.textContent = time;
-    if (time <= 0)
-        quizEnd();
-}
-
-
-
-
-
-    function displayQuestions() {
-        console.log(questions[index].question)
-        quiz.textContent = questions[index].question;
-        for (var i = 0; i < questions[index].answers.length; i++) {
-            //create button for each answer
-            var choiceBtn = document.createElement("BUTTON");
-            //display each of the answers onto the button
-            choiceBtn.textContent = questions[0].answers[i]
-            choices.appendChild(choiceBtn)
-            timer();
+            time--;
+            timerEl.textContent = time;
+            if (time <= 0)
+                quizEnd();
         }
-    }
 
-    
+
+
+
+
+function displayQuestions() {
+            console.log(questions[index].question)
+            quiz.textContent = questions[index].question;
+            for (var i = 0; i < questions[index].answers.length; i++) {
+                //create button for each answer
+                var choiceBtn = document.createElement("BUTTON");
+                //display each of the answers onto the button
+                choiceBtn.textContent = questions[0].answers[i]
+                choices.appendChild(choiceBtn)
+                timer();
+            }
+        }
+}
